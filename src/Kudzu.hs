b16911c6bd2ecf81d037bafa7956e14674b39b3e
Shae Erisson <shae@scannedinavian.com>
2023-09-07 13:08:38 -0500

    clean up docstrings, remove spurious comments

--- src/Kudzu.hs
+++ src/Kudzu.hs
@@ -17,14 +17,12 @@ testUntilSameQCMany howMany ts = do
 -- | QuickCheck
 testUntilSameQC :: QC.Testable a => Int -> a -> IO (Int, Maybe Integer)
 testUntilSameQC n testable = do
-  let rs = map (examineAndCount' testable) [0..n] -- examineAndCount' <$> repeat testable
+  let rs = map (examineAndCount' testable) [0..n]
   r1 <- head rs
   grabUntilNSame 0 n n (tail rs) r1
 
 examineAndCount' :: QC.Testable prop => prop -> Int -> IO Integer
 examineAndCount' v size = do
-  -- poor QC, you got problems
-  -- quickCheckWith (stdArgs {maxSize = 400, maxSuccess = 1}) v
   qcg <- QC.newQCGen
   QC.quickCheckWith (QC.stdArgs { QC.replay = Just (qcg, size)}) (QC.withMaxSuccess 1 v)
   tixModuleCount <$> examineTix
@@ -46,11 +44,11 @@ examineAndCountHH prop = do
   unless passed $ error "property failed"
   tixModuleCount <$> examineTix
 
+-- | LeanCheck
 testUntilSameLCMany :: (Traversable t, LC.Testable a) => Int -> t a -> IO (t (Int, Maybe Integer))
 testUntilSameLCMany howMany ts = do
   mapM (testUntilSameLC howMany) ts
 
--- | LeanCheck
 testUntilSameLC :: LC.Testable a => Int -> a -> IO (Int, Maybe Integer)
 testUntilSameLC n testable = do
   let rs = examineAndCount <$> LC.results testable
@@ -69,10 +67,10 @@ grabUntilNSame c orig n (a : as) z = do
     then grabUntilNSame (c + 1) orig (n - 1) as z
     else grabUntilNSame (c + 1) orig orig as a'
 
--- How many regions were executed at least once for this module?
+-- | How many regions were executed at least once for this module?
 tixCount :: TixModule -> Integer
 tixCount (TixModule _ _ _ regions) = sum $ 1 <$ filter (> 0) regions
 
--- How many regions were executed at least once for all these modules?
+-- | How many regions were executed at least once for all these modules?
 tixModuleCount :: Tix -> Integer
 tixModuleCount (Tix ms) = sum $ map tixCount ms
